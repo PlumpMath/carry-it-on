a88ac4334d35c5fca542ddcb3668257e42b9d284
Stefano Fadda <stefanofaddajob@gmail.com>
2017-11-13 23:06:09 +0100

    no message

---
+++ Assets/Scripts/CarillonControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CarillonControl : MonoBehaviour {
+
+	public float speed = 0f;
+	public float speedIncrement = 10f;
+	public float speedDecrement = 0.5f;
+	public float maxSpeed = 400f;
+
+	// Update is called once per frame
+	void Update () {
+		//float handleRotationX = transform.rotation.eulerAngles.x;
+
+		if (Input.GetKeyDown ("up") && speed < maxSpeed) {
+			speed = speed + speedIncrement;
+
+		}else if (Input.GetKeyDown ("down") && speed < maxSpeed && speed > 0) {
+			speed = speed - speedIncrement;
+
+		} else if(speed > 0) {
+			speed = speed - speedDecrement;
+
+		}
+
+		transform.Rotate (Vector3.right, speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	public Vector3 direction;
+	public float speed = 0f;
+	public float dragSpeed = 40f;
+
+	public float pitchMin = 0.6f;
+	public float pitchMax = 2f;
+
+	private float diff; 
+	private float pitchVal;
+	private float speedRatio;
+
+	private GameObject handleGameObject;
+	private CarillonControl handleScript;
+
+	private AudioSource lullaby;
+
+	void Start(){
+		handleGameObject = GameObject.FindGameObjectWithTag ("Handle");
+		handleScript = handleGameObject.GetComponent<CarillonControl> ();
+
+		lullaby = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (handleScript.speed > 0) {
+			if (!lullaby.isPlaying) {
+				lullaby.Play ();
+			}
+
+			diff = handleScript.speed - dragSpeed;
+			if (diff > 0) {
+				speed = diff;
+			} else {
+				speed = handleScript.speed;
+			}
+
+
+			speedRatio = (speed / handleScript.maxSpeed);
+			pitchVal = (pitchMax - pitchMin) * speedRatio + pitchMin;
+
+			if (pitchVal > 0.95f && pitchVal < 1.05f) {
+				lullaby.pitch = 1f;
+			} else {
+				lullaby.pitch = pitchVal;
+			}
+
+			transform.Rotate(direction, speed * Time.deltaTime);
+
+		}
+
+		if (handleScript.speed < 1 && lullaby.isPlaying) {
+			lullaby.Pause ();
+		}
+	}
+}
