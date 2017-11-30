2f58163351cb80e49812bc4c2a941542b627c910
Stefano Fadda <stefanofaddajob@gmail.com>
2017-11-30 23:51:32 +0100

    no message

--- Assets/Scripts/CarillonControl.cs
+++ Assets/Scripts/CarillonControl.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 
 public class CarillonControl : MonoBehaviour {
-
+	public Vector3 direction;
 	public float speed = 0f;
-	public float speedIncrement = 10f;
+	public float speedIncrement = 8f;
 	public float speedDecrement = 0.5f;
 	public float maxSpeed = 400f;
-
+	private float tempSpeed = 0f;
 	// Update is called once per frame
 	void Update () {
 		//float handleRotationX = transform.rotation.eulerAngles.x;
@@ -14,14 +14,17 @@ public class CarillonControl : MonoBehaviour {
 		if (Input.GetKeyDown ("up") && speed < maxSpeed) {
 			speed = speed + speedIncrement;
 
-		}else if (Input.GetKeyDown ("down") && speed < maxSpeed && speed > 0) {
-			speed = speed - speedIncrement;
+		}else if (Input.GetKeyDown ("down")) {
+			tempSpeed = speed - speedIncrement;
+			if (tempSpeed > 0) {
+				speed = tempSpeed;
+			}
 
 		} else if(speed > 0) {
 			speed = speed - speedDecrement;
 
 		}
 
-		transform.Rotate (Vector3.right, speed * Time.deltaTime);
+		transform.Rotate (direction, speed * Time.deltaTime);
 	}
 }
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -1,27 +1,57 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Rotate : MonoBehaviour {
 	public Vector3 direction;
 	public float speed = 0f;
-	public float dragSpeed = 40f;
-
+	public float dragSpeed = 0.3f;
 	public float pitchMin = 0.6f;
 	public float pitchMax = 2f;
 
 	private float diff; 
 	private float pitchVal;
 	private float speedRatio;
-
 	private GameObject handleGameObject;
 	private CarillonControl handleScript;
+	private GameObject zParticleGameObject;
+	private ParticleSystem zParticle;
 
 	private AudioSource lullaby;
 
-	void Start(){
+	private GameObject arrowUpGameObject;
+	private GameObject arrowDownGameObject;
+	private Text scoreText;
+	private Text failText;
+
+	private int score;
+	private bool timed;
+	private float currentTimer;
+	private float failCounter;
+	private bool fail;
+	private float failMax = 2f;
+
+	void Awake(){
 		handleGameObject = GameObject.FindGameObjectWithTag ("Handle");
 		handleScript = handleGameObject.GetComponent<CarillonControl> ();
-
 		lullaby = GetComponent<AudioSource> ();
+
+		zParticleGameObject = GameObject.FindGameObjectWithTag ("ZParticle");
+		zParticle = zParticleGameObject.GetComponent<ParticleSystem> ();
+
+		arrowUpGameObject = GameObject.FindGameObjectWithTag ("ArrowUp");
+		arrowDownGameObject = GameObject.FindGameObjectWithTag ("ArrowDown");
+		arrowUpGameObject.SetActive (false);
+		arrowDownGameObject.SetActive (false);
+
+		scoreText = GameObject.FindGameObjectWithTag ("Score").GetComponent<Text>();
+		failText = GameObject.FindGameObjectWithTag ("Fails").GetComponent<Text>();
+
+		score = 0;
+		currentTimer = 1f;
+		timed = false;
+		failCounter = failMax;
+		fail = false;
 	}
 	
 	// Update is called once per frame
@@ -31,29 +61,82 @@ public class Rotate : MonoBehaviour {
 				lullaby.Play ();
 			}
 
-			diff = handleScript.speed - dragSpeed;
-			if (diff > 0) {
-				speed = diff;
-			} else {
-				speed = handleScript.speed;
-			}
-
-
+			speed = handleScript.speed - (handleScript.speed * dragSpeed);
 			speedRatio = (speed / handleScript.maxSpeed);
 			pitchVal = (pitchMax - pitchMin) * speedRatio + pitchMin;
 
-			if (pitchVal > 0.95f && pitchVal < 1.05f) {
+			if (pitchVal > 0.9f && pitchVal < 1.1f) {
 				lullaby.pitch = 1f;
+				if(!zParticle.isPlaying){
+					zParticle.Play ();
+				}
+				Scoring ();
+				FailReset ();
 			} else {
+				zParticle.Stop();
 				lullaby.pitch = pitchVal;
+				FailDecrease ();
 			}
 
 			transform.Rotate(direction, speed * Time.deltaTime);
 
 		}
 
+		ManageArrows (pitchVal);
+
 		if (handleScript.speed < 1 && lullaby.isPlaying) {
 			lullaby.Pause ();
+		}
+
+		Fail ();
+	}
+
+	void FailReset(){
+		fail = true;
+		failCounter = failMax;
+		failText.text = "";
+	}
+
+	void FailDecrease(){
+		if (fail) {
+			failCounter -= Time.deltaTime;
+			if (failCounter < 0f) {
+				failCounter = 0f;
+			}
+			failText.text = failCounter.ToString ("N2");
+		}
+	}
+
+	void Fail(){
+		if (failCounter == 0) {
+			PlayerPrefs.SetInt ("highscore", score);
+			SceneManager.LoadScene(3);
+		}
+	}
+
+	void ManageArrows(float val){
+		if (val < 0.9f) {
+			arrowUpGameObject.SetActive (true);
+		} else if(val > 1.1f){
+			arrowDownGameObject.SetActive (true);
+		} else {
+			arrowUpGameObject.SetActive (false);
+			arrowDownGameObject.SetActive (false);
+		}
+	}
+
+	void Scoring(){
+		if (timed) {
+			currentTimer -= Time.deltaTime;
+		}
+
+		if (currentTimer < 0) {
+			currentTimer = 1f;
+			timed = false;
+			score++;
+			scoreText.text = score.ToString("0");
+		} else {
+			timed = true;
 		}
 	}
 }
